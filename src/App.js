d07ff15de06ce438fc51c709905412d025eb48cc
Grigor Minasyan <grigorminasyan7@gmail.com>
2021-08-27 18:49:44 -0700

    final

--- src/App.js
+++ src/App.js
@@ -1,79 +1,86 @@
-import React, { useState } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Typography from '@material-ui/core/Typography';
-import Container from '@material-ui/core/Container';
-import FormControl from '@material-ui/core/FormControl';
-import purple from '@material-ui/core/colors/purple';
-import Paper from '@material-ui/core/Paper';
-import TextField from '@material-ui/core/TextField';
+import React, { useState } from "react";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+import FormControl from "@material-ui/core/FormControl";
+import purple from "@material-ui/core/colors/purple";
+import Paper from "@material-ui/core/Paper";
+import TextField from "@material-ui/core/TextField";
 
-import secret from './secret.js';
-import AES from 'crypto-js/aes';
-import enc_utf8 from 'crypto-js/enc-utf8';
+import secret from "./secret.js";
+import AES from "crypto-js/aes";
+import enc_utf8 from "crypto-js/enc-utf8";
 
-import { ThemeProvider, createTheme, makeStyles } from '@material-ui/core/styles';
+import {
+  ThemeProvider,
+  createTheme,
+  makeStyles,
+} from "@material-ui/core/styles";
 
 const theme = createTheme({
-	typography: {
-		fontFamily: ['"Patrick Hand"', 'cursive'].join(','),
-		fontSize: 15,
-	},
-	palette: {
-		primary: purple,
-	},
+  typography: {
+    fontFamily: ['"Patrick Hand"', "cursive"].join(","),
+    fontSize: 17,
+  },
+  palette: {
+    primary: purple,
+  },
 });
 
 const useStyles = makeStyles((theme) => ({
-	root: {
-		'& > *': {
-			margin: theme.spacing(1),
-			padding: theme.spacing(2),
-		},
-	},
+  root: {
+    "& > *": {
+      //   margin: theme.spacing(1),
+      marginTop: theme.spacing(2),
+      marginBottom: theme.spacing(2),
+      padding: theme.spacing(2),
+    },
+  },
 }));
 
 function App() {
-	const classes = useStyles();
-	const [password, setPassword] = useState('');
-	const [letter, setLetter] = useState('');
-	const handleChange = (e) => {
-		const value = e.target.value;
-		let decrypted = '';
-		try {
-			const bytes = AES.decrypt(secret, value);
-			const decrypted_test = bytes.toString(enc_utf8);
-			decrypted = decrypted_test;
-		} catch (error) {}
-		setLetter(decrypted);
-		setPassword(value);
-	};
-	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<Container maxWidth="md" className={classes.root}>
-				<Paper elevation={15}>
-					<FormControl fullWidth variant="outlined">
-						<TextField
-							onChange={handleChange}
-							value={password}
-							label="Enter the password to see the letter"
-							type="password"
-							variant="outlined"
-						/>
-					</FormControl>
+  const classes = useStyles();
+  const [password, setPassword] = useState("");
+  const [letter, setLetter] = useState("");
+  const handleChange = (e) => {
+    const value = e.target.value;
+    let decrypted = "";
+    try {
+      const bytes = AES.decrypt(secret, value);
+      const decrypted_test = bytes.toString(enc_utf8);
+      decrypted = decrypted_test;
+    } catch (error) {}
+    decrypted = decrypted.length < 5 ? "" : decrypted;
+    setLetter(decrypted);
+    setPassword(value);
+  };
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="md" className={classes.root}>
+        <Paper elevation={15}>
+          <FormControl fullWidth variant="outlined">
+            <TextField
+              onChange={handleChange}
+              value={password}
+              label="Enter the password to see the letter"
+              type="password"
+              variant="outlined"
+            />
+          </FormControl>
 
-					<Typography
-						variant="body1"
-						gutterBottom
-						align="left"
-						style={{ whiteSpace: 'pre-wrap' }}
-					>
-						{letter}
-					</Typography>
-				</Paper>
-			</Container>
-		</ThemeProvider>
-	);
+          <Typography
+            variant="body1"
+            gutterBottom
+            align="center"
+            style={{ whiteSpace: "pre-wrap" }}
+          >
+            {letter}
+          </Typography>
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default App;
--- src/generateSecretMessage.example.js
+++ src/generateSecretMessage.example.js
@@ -1,12 +1,11 @@
 const message = `
 	Enter your secret message here.
 `;
-const fs = require('fs');
-const AES = require('crypto-js/aes');
-const key = 'enterDecryptionPasswordHere';
-const ciphertext = AES.encrypt(message, key).toString();
+const key = "enterDecryptionPasswordHere";
 
-fs.writeFileSync(
-	__dirname + '/secret.js',
-	`const secret = \`${ciphertext}\`;\nexport default secret;\n`
+const ciphertext = require("crypto-js/aes").encrypt(message, key).toString();
+
+require("fs").writeFileSync(
+  __dirname + "/secret.js",
+  `const secret = \`${ciphertext}\`;\nexport default secret;\n`
 );
